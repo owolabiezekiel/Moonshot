f67bcf584be4fc6c104435153a4518339cc9b102
Owolabi Tobiloba <owo.ezekiel@gmail.com>
2021-05-07 00:47:56 +0100

    Initial Commit

---
+++ MoonShot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoonShot
+//
+//  Created by Tobiloba Owolabi on 07/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Tobiloba Owolabi on 07/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
